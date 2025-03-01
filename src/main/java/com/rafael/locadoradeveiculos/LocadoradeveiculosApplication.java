3c4ca1e03616dd0b3fd478110825cfdf4b5302fd
Rafael Martinez <rafael.loamartinez@gmail.com>
2025-03-01 16:09:54 -0300

    first commit

---
+++ src/main/java/com/rafael/locadoradeveiculos/LocadoradeveiculosApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.locadoradeveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoradeveiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoradeveiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafael/locadoradeveiculos/LocadoradeveiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.locadoradeveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoradeveiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
